432d2766c67e70616e2b1b7abc1c5e364a54e73e
Josh Gulledge <jgulledge19@outlook.com>
2018-09-27 10:35:49 -0400

    Add in the ImageTransformationException class

---
+++ src/Exceptions/ImageTransformationException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshgulledge
+ * Date: 9/27/18
+ * Time: 10:11 AM
+ */
+
+namespace LCI\Salsify\Exceptions;
+
+
+class ImageTransformationException extends \Exception
+{
+
+}
